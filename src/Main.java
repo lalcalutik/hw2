8675355ea9915012182c8760832a802c4ecde480
ПК <themorozushka@gmail.com>
2022-12-21 12:26:17 +0300

    Проверьте ДЗ

--- src/Main.java
+++ src/Main.java
@@ -24,9 +24,9 @@ public class Main {
         var dog = 0.8;
         var cat = 3.6;
         var paper = 0.763789;
-         dog = dog + 4;
-         cat = cat + 4;
-         paper = paper +4;
+        dog = dog + 4;
+        cat = cat + 4;
+        paper = paper +4;
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
